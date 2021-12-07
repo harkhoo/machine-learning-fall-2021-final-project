a9179f10272b9d8c52a86e0a9201a54250fa30e7
Dimitri Lezcano <dlezcan1@jhu.edu>
2021-12-07 10:52:41 -0500

    Added FocalLoss and updated training

--- src/models.py
+++ src/models.py
@@ -1,20 +1,21 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
 
-class FeedForward(torch.nn.Module):
-    def __init__(self):
-        super(FeedForward, self).__init__()
-        self.fc1 = nn.Linear(100, 10)
-        self.fc2 = nn.Linear(10, 1)
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
+class FeedForward( nn.Module ):
+    def __init__( self ):
+        super( FeedForward, self ).__init__()
+        self.fc1 = nn.Linear( 100, 10 )
+        self.fc2 = nn.Linear( 10, 1 )
+
+    # __init__
+
+    def forward( self, x ):
+        x = self.fc1( x )
+        x = F.relu( x )
+        x = self.fc2( x )
 
         return x
 
+    # forward
+# class: FeedForward
